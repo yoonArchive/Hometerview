9e4a197d78318b917b278b028e444b051decb47d
giyoon27 <giyoon27@naver.com>
2022-07-24 23:58:48 +0900

    fix: ApplyRepositorySupport 불필요한 import 삭제

--- backend/src/main/java/com/ssafy/db/repository/ApplyRepositorySupport.java
+++ backend/src/main/java/com/ssafy/db/repository/ApplyRepositorySupport.java
@@ -3,8 +3,6 @@ package com.ssafy.db.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.db.entity.Apply;
 import com.ssafy.db.entity.QApply;
-import com.ssafy.db.entity.QUser;
-import com.ssafy.db.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
