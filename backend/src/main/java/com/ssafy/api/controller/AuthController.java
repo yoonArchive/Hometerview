a5c6e7fd3210a105fcb96b3466ad98da746f6714
giyoon27 <giyoon27@naver.com>
2022-07-26 12:50:23 +0900

    fix: AuthController 불필요한 import 삭제

--- backend/src/main/java/com/ssafy/api/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/api/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.ssafy.api.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
