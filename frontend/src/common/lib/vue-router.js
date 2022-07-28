96e6a0870c2a7a130c5867b4524b1ba458bb9ae8
DESKTOP-KVCQHCD\SSAFY <moonjo960@gmail.com>
2022-07-28 11:09:09 +0900

    fix : router fix

--- frontend/src/common/lib/vue-router.js
+++ frontend/src/common/lib/vue-router.js
@@ -128,9 +128,6 @@ function makeRoutesFromMenu () {
       name : 'passwordconfirm',
       component : PasswordConfirm,
     },{
-<<<<<<< HEAD
-      path: 'coverletter/:resumeindex',
-=======
       path : 'createmembersrecruitment',
       name : 'createmembersrecruitment',
       component : CreateMembersRecruitmentView,
@@ -148,7 +145,6 @@ function makeRoutesFromMenu () {
       component : MembersRecruitmentEditView,
     },{
       path: 'coverletter/:resumeNo',
->>>>>>> 9274e7143c76440cf01544e836aae4e960414343
       name : 'coverletter',
       component : CoverLetterView,
     },{
