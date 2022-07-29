13ea5f8a8e88d6f9be68d91277978c3527fd91b0
jayho-k <sox3579@gmail.com>
2022-07-29 14:15:14 +0900

    style : 모집글 상세페이지 디자인 틀 적용

--- frontend/src/views/membersrecruitment/MembersRecruitmentDetailView.vue
+++ frontend/src/views/membersrecruitment/MembersRecruitmentDetailView.vue
@@ -1,21 +1,38 @@
 <template>
  <h1>Detail Page</h1>
-    <div>
-      대표 이미지 : 
-      <img src="{{ recruitDetail.stdImg }}" alt=""><br>
+
+    <div class="container">
+      <h2 class="py-3">
+        {{ recruitDetail.recruitTitle }}
+      </h2>
+      <div class="d-flex justify-content-around">
+        <div>
+          대표 이미지 : 
+          <img src="{{ recruitDetail.stdImg }}" alt="">
+        </div>
+        <div>
+          스터디 이름 :{{ recruitDetail.stdName }} <br>
+          활동기간 : {{ recruitDetail.startDate }} ~  {{ recruitDetail.endDate }}<br>
+          진행 일자 : {{ recruitDetail.stdDay }} <br>
+          모집인원 :   {{ recruitDetail.stdLimit }}<br>
+          스터디 타입 : {{ recruitDetail.stdType }} <br>
+        </div>
+      </div><hr>
+      <div class="card border-secondary mb-3" style="max-width: 80rem; height: 20rem;">
+        <div class="card-header">스터디 소개</div>
+        <div class="card-body text-secondary">
+          <h5 class="card-title">{{ recruitDetail.stdName }}</h5>
+          <p class="card-text">{{ recruitDetail.stdDetail }}</p>
+        </div>
+      </div>
+      기업명 : {{ recruitDetail.comName }}
+      스터디 현황 : {{ recruitDetail.recruitStatus }} 
+      <button @click="moveToUpdate">수정</button>
+      <button @click="deleteRecruitmentDetail([recruitNo])"> 삭제 </button>
+      <button type="button" class="btn btn-bd-primary" style="color:indigo">Primary</button>
+
+
     </div>
-    스터디 이름 : {{ recruitDetail.stdName }} <br>
-    모집인원 : {{ recruitDetail.stdLimit }} <br>
-    활동기간 : {{ recruitDetail.startDate }} ~  {{ recruitDetail.endDate }}<br>
-    모집 타이틀 : {{ recruitDetail.recruitTitle }} <br>
-    스터디 타입 : {{ recruitDetail.stdType }} <br>
-    기업명 : {{ recruitDetail.comName }} <br>
-    진행 일자 : {{ recruitDetail.stdDay }} <br>
-    스터디 현황 : {{ recruitDetail.recruitStatus }} <br>
-    스터디 소개 : <br>
-    {{ recruitDetail.stdDetail }} <br>
-    <button @click="moveToUpdate">수정</button>
-    <button @click="deleteRecruitmentDetail([recruitNo])"> 삭제 </button>
 
 </template>
 
@@ -66,5 +83,37 @@
 </script>
 
 <style>
+  .btn-bd-primary {
+    --bs-btn-font-weight: 600;
+    --bs-btn-color: var(--bs-white);
+    --bs-btn-bg: var(--bd-violet);
+    --bs-btn-border-color: var(--bd-violet);
+    --bs-btn-border-radius: .5rem;
+    --bs-btn-hover-color: var(--bs-white);
+    /* --bs-btn-hover-bg: #{shade-color($bd-violet, 10%)}; */
+    /* --bs-btn-hover-border-color: #{shade-color($bd-violet, 10%)}; */
+    --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
+    --bs-btn-active-color: var(--bs-btn-hover-color);
+    /* --bs-btn-active-bg: #{shade-color($bd-violet, 20%)}; */
+    /* --bs-btn-active-border-color: #{shade-color($bd-violet, 20%)}; */
+  }
+
+
+  .recruitmentInfo{
+    width: 440px;
+    height: 300px;
+    background: #FFFFFF;
+    box-shadow: 4px 4px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 30px;
+  }
+  .recruitment-intro{
+
+    width: 841px;
+    height: 287px;
+
+    background: #FFFFFF;
+    border: 1px solid #000000;
+    border-radius: 30px;
+  }
 
 </style>
--- frontend/src/views/membersrecruitment/components/CreateMembersRecruitmentForm.vue
+++ frontend/src/views/membersrecruitment/components/CreateMembersRecruitmentForm.vue
@@ -1,50 +1,54 @@
 <template>
-  
-  <form @submit.prevent="submitType(action)">
-    <div>
-      <label for="recruitTitle">모집글 제목 : </label>
-      <input v-model="newrecruitmentInfo.recruitTitle" type="text" id="recruitTitle" />
-    </div>
-    <div>
-      카테고리 :
-      <label for="stdType">기업 면접</label>
-      <input v-model="newrecruitmentInfo.stdType" name="stdType" type="radio" id="stdType" value="COM" @click="isCompany('COM')" checked/>
-      <label for="stdType">자율 면접</label>
-      <input v-model="newrecruitmentInfo.stdType" name="stdType" type="radio" id="stdType" value="FREE" @click="isCompany('FREE')"/>
-    </div>
-    <div>
-      <label for="comName">기업명 : </label>
-      <input v-model="newrecruitmentInfo.comName" type="text" id="comName" :disabled="company"/>
-    </div>
-    <div>
-      <label for="startDate"> 시작일 : </label>
-      <input v-model="newrecruitmentInfo.startDate" type="date" id="startDate" />
-      <label for="endDate">종료일 : </label>
-      <input v-model="newrecruitmentInfo.endDate" type="date" id="endDate">
-    </div>
-    <div>
-      <label for="stdDay">진행 일시 : </label>
-      <input v-model="newrecruitmentInfo.stdDay" type="text" id="stdDay" placeholder="예) 매주 월, 수, 금  오후 10시 ( 협의가능 )" />
-    </div>
-    <div>
-      <label for="stdLimit"> 모집 인원 : </label>
-      <input v-model="newrecruitmentInfo.stdLimit" type="number" id="stdLimit">
-    </div>
-    <div>
-      <label for="stdName">스터디 이름 : </label>
-      <input v-model="newrecruitmentInfo.stdName" type="text" id="stdName">
-    </div>
-    <div>
-      <label for="stdDetail"> 스터디 설명 : </label>
-      <input v-model="newrecruitmentInfo.stdDetail" type="text" id="stdDetail">
-    </div>
-    <div>
-      <label for="stdImg">이미지 : </label>
-      <input multiple @change="onInputimage" ref="studyImage" type="file">
-      <input v-model="newrecruitmentInfo.stdImg" type="image" id="stdImg">
-    </div>
-    <button>{{ action }}</button>
-  </form>
+    <h4 class="py-2" style="color: #653FD3;">스터디 그룹을 만들어 보세요!</h4>
+    <form @submit.prevent="submitType(action)">
+      <div class="py-2">
+        <label class="font-color" for="recruitTitle">모집글 제목 : </label>
+        <input class="box1" v-model="newrecruitmentInfo.recruitTitle" type="text" id="recruitTitle" />
+      </div>
+      <div class="py-2">
+        <span class="font-color"> 카테고리 : </span> 
+        <label for="stdType">기업 면접</label>
+        <input v-model="newrecruitmentInfo.stdType" name="stdType" type="radio" id="stdType" value="COM" @click="isCompany('COM')" checked/>
+        <label for="stdType">자율 면접</label>
+        <input v-model="newrecruitmentInfo.stdType" name="stdType" type="radio" id="stdType" value="FREE" @click="isCompany('FREE')"/>
+      </div>
+      <div class="py-2">
+        <label class="font-color" for="comName">기업명 : </label>
+        <input class="box1" v-model="newrecruitmentInfo.comName" type="text" id="comName" :disabled="company"/>
+      </div>
+      <div class="py-2">
+        <label class="font-color" for="startDate"> 시작일 : </label>
+        <input v-model="newrecruitmentInfo.startDate" type="date" id="startDate" />
+        <label class="font-color" for="endDate">종료일 : </label>
+        <input v-model="newrecruitmentInfo.endDate" type="date" id="endDate">
+      </div>
+      <div class="py-2">
+        <label class="font-color" for="stdDay">진행 일시 : </label>
+        <input class="box1" v-model="newrecruitmentInfo.stdDay" type="text" id="stdDay" placeholder="예) 매주 월, 수, 금  오후 10시 ( 협의가능 )" />
+      </div>
+      <div class="py-2">
+        <label class="font-color" for="stdLimit"> 모집 인원 : </label>
+        <input v-model="newrecruitmentInfo.stdLimit" type="number" id="stdLimit">
+      </div>
+      <div class="py-2">
+        <label class="font-color" for="stdName">스터디 이름 : </label>
+        <input class="box1" v-model="newrecruitmentInfo.stdName" type="text" id="stdName">
+      </div>
+      <div>
+        <div class="py-2">
+          <label class="font-color" for="stdDetail"> 스터디 설명 : </label>
+        </div>
+        <div>
+          <input class="box2" v-model="newrecruitmentInfo.stdDetail" type="text" id="stdDetail">
+        </div>
+      </div>
+      <div>
+        <label for="stdImg">이미지 : </label>
+        <input multiple @change="onInputimage" ref="studyImage" type="file">
+        <input v-model="newrecruitmentInfo.stdImg" type="image" id="stdImg">
+      </div>
+      <button>{{ action }}</button>
+    </form>
     <!-- <button @click="deleteRecruitmentDetail(recruitNo)">삭제 </button> -->
   
 </template>
@@ -112,5 +116,44 @@
 </script>
 
 <style>
+  .create-form{
+    width: 550px;
+    height: 1000px;
 
+    background: #F3F4FF;
+  }
+  .font-color{
+    width: 130px;
+    height: 24px;
+    font-family: 'ABeeZee';
+    font-style: normal;
+    font-weight: 1000;
+    font-size: 20px;
+    line-height: 150%;
+    color: #653FD3;
+  }
+  .box1{
+    width: 381px;
+    height: 38px;
+
+    /* Gray/White */
+
+    background: #FFFFFF;
+    /* Gray/400 */
+
+    border: 1px solid #CED4DA;
+    border-radius: 4px;
+  }
+  .box2{
+    width: 381px;
+    height: 300px;
+
+    /* Gray/White */
+
+    background: #FFFFFF;
+    /* Gray/400 */
+
+    border: 1px solid #CED4DA;
+    border-radius: 4px;
+  }
 </style>
