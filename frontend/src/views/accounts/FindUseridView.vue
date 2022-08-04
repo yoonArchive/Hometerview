ba0e62a88ab575d72b72071208438565b156cabd
giyoon27 <giyoon27@naver.com>
2022-08-04 12:46:09 +0900

    feat: 비밀번호 찾기 페이지 디자인

--- frontend/src/views/accounts/FindUseridView.vue
+++ frontend/src/views/accounts/FindUseridView.vue
@@ -4,7 +4,6 @@
       <div class="row full-height justify-content-center">
         <div class="col-12 text-center align-self-center py-5">
           <div class="section pb-5 pt-5 pt-sm-2 text-center">
-            <label for="reg-log"></label>
             <div class="card-3d-wrap mx-auto">
               <div class="card-3d-wrapper">
                 <div class="card-front">
