a4651948af500fc3728252d30b12860719540300
DESKTOP-KVCQHCD\SSAFY <moonjo960@gmail.com>
2022-08-09 18:07:30 +0900

    feature : TTS 음원 저장

--- frontend/server.js
+++ frontend/server.js
@@ -5,37 +5,27 @@ app.listen(9002, function(){
   console.log('listening on 9002')
 });
 
-app.get('/ttsrequest', function(req, res){
+app.post('/ttsrequest', async (req, res) => {
   const textToSpeech = require('@google-cloud/text-to-speech')
-
+  console.log(req);
   // dot env
 
   require('dotenv').config()
-
   const fs = require('fs')
-
   const util = require('util')
-
   const client = new textToSpeech.TextToSpeechClient()
-
-  async function convertTextToMp3(){
-      const text = "Hellow World!!";
-
-      const request = {
-      input : {text:text},
-      voice : {languageCode:'en-US', ssmlGender:'NEUTRAL'},
-      audioConfig:{audioEncoding : 'MP3'}
-      }
-
-      const [response] = await client.synthesizeSpeech(request);
-
-      const writeFile = util.promisify(fs.writeFile)
-
-      await writeFile("output.mp3",response.audioContent,'binary')
-
-      console.log('Text to Speech has completed. Audio file has been saved');
-  }
-
-  convertTextToMp3()
-  return;
+  const text = "test voice";
+  const request = {
+    input : {text:text},
+    voice : {languageCode:'en-US', ssmlGender:'NEUTRAL'},
+    audioConfig: {audioEncoding : 'MP3'}
+  };
+  const [response] = await client.synthesizeSpeech(request);
+  const writeFile = util.promisify(fs.writeFile);
+  await writeFile("output.mp3",response.audioContent,'binary');
+  console.log(response);
+  console.log('Text to Speech has completed. Audio file has been saved');
+  let responseData = response.audioContent;
+  console.log(responseData);
+  res.send(responseData);
 })
