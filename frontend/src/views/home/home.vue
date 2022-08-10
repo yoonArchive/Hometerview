3ddf74cef526dcdb2681879f69e549652cc9aaf5
HangHang <dhkdwlsgod@naver.com>
2022-08-10 22:55:14 +0900

    fix: 홈.vue scoped 해제

--- frontend/src/views/home/home.vue
+++ frontend/src/views/home/home.vue
@@ -151,7 +151,7 @@ export default {
   }
 };
 </script>
-<style scoped>
+<style>
 @import url("https://fonts.googleapis.com/css?family=Muli:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&subset=latin-ext,vietnamese");
 @import url("https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&subset=latin-ext");
 
