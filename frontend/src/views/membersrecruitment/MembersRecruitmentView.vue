e70265f9a20a8509384e72305bbf5aa4d56d003b
giyoon27 <giyoon27@naver.com>
2022-08-14 16:19:38 +0900

    fix: 스터디 모집글 슬라이더 이동 버튼 위치 수정

--- frontend/src/views/membersrecruitment/MembersRecruitmentView.vue
+++ frontend/src/views/membersrecruitment/MembersRecruitmentView.vue
@@ -116,24 +116,6 @@
           </button>
         </div>
       </div>
-      <!-- <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleCaptions"
-        data-bs-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleCaptions"
-        data-bs-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button> -->
     </div>
     <div class="row" style="margin-bottom:20px;">
       <div class="title col-md-3">
